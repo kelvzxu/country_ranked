ba1a3ffa499a9bc181e83dbdc347c5908b5d0f68
kelzxu-witech <kelvin@witech.co.id>
2024-11-30 15:49:21 +0700

    [ADD] feature country list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import HomePage from './pages/HomePage';
+import ComparePage from './pages/ComparePage';
+import NewsPage from './pages/NewsPage';
+import CountryDetailPage from './pages/CountryDetailPage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/compare" element={<ComparePage />} />
+        <Route path="/news" element={<NewsPage />} />
+        <Route path="/country/:name" element={<CountryDetailPage />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
