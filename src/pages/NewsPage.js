685ae5fae9e021c638406f7d8cc0b74ea758300d
kelzxu-witech <kelvin@witech.co.id>
2024-12-11 09:03:43 +0700

    [ADD] add page news

--- src/pages/NewsPage.js
+++ src/pages/NewsPage.js
@@ -1,48 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import Sidebar from '../components/Sidebar';
+import MainContent from '../components/newsContent';
+import Footer from '../components/Footer';
+import { fetchNews } from '../store/newsSlice';
+import "./styles/NewsPage.css";
 
 const NewsPage = () => {
+  const { category } = useParams(); // Ambil parameter category dari URL
+  const dispatch = useDispatch();
+  const { articles, loading, error } = useSelector(state => state.news);
+
+  useEffect(() => {
+    // Dispatch action fetchNews ketika category berubah
+    dispatch(fetchNews(category || 'home')); // Default ke 'home' jika tidak ada category
+  }, [category, dispatch]);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
   return (
     <div>
-      <h1>News & Articles</h1>
-      {/* Konten utama untuk halaman News/Articles */}
+      <div className="d-flex">
+        <Sidebar /> {/* Sidebar di sebelah kiri */}
+        <div className="flex-grow-1"> {/* Konten utama mengambil sisa ruang */}
+          <MainContent articles={articles} />
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
 
 export default NewsPage;
-// import React, { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { useParams } from 'react-router-dom';
-// import Sidebar from '../components/Sidebar';
-// import MainContent from '../components/newsContent';
-// import Footer from '../components/Footer';
-// import { fetchNews } from '../store/newsSlice';
-// import "./styles/NewsPage.css";
-
-// const NewsPage = () => {
-//   const { category } = useParams(); // Ambil parameter category dari URL
-//   const dispatch = useDispatch();
-//   const { articles, loading, error } = useSelector(state => state.news);
-
-//   useEffect(() => {
-//     // Dispatch action fetchNews ketika category berubah
-//     dispatch(fetchNews(category || 'home')); // Default ke 'home' jika tidak ada category
-//   }, [category, dispatch]);
-
-//   if (loading) return <div>Loading...</div>;
-//   if (error) return <div>Error: {error}</div>;
-
-//   return (
-//     <div>
-//       <div className="d-flex">
-//         <Sidebar /> {/* Sidebar di sebelah kiri */}
-//         <div className="flex-grow-1"> {/* Konten utama mengambil sisa ruang */}
-//           <MainContent articles={articles} />
-//         </div>
-//       </div>
-//       <Footer />
-//     </div>
-//   );
-// };
-
-// export default NewsPage;
